bf3d66c267f0f7938ea3f65959aa74b06d01fcc4
Jose Fernando Guerra Muñoz <fernandoguerra344@gmail.com>
2026-07-31 19:05:16 -0600

    first public repo version

--- src/core/Menu.ts
+++ src/core/Menu.ts
@@ -104,7 +104,7 @@ export class Menu {
                 await ctx.notifier.reportError(label, error, ctx);
 
                 logger.error(errorMessage(error));
-                ctx.params.catch === "throw" && process.exit(1);
+                if(ctx.params.catch === "throw" || ctx.flags.catch === "throw") throw error;
             }
 
             return;
